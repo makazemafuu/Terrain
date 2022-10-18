55b2d95b99f75ddc1684449915ac06f6903756b0
Diane <102394629+makazemafuu@users.noreply.github.com>
2022-10-18 15:24:48 +0200

    Fournir un moyen d'évaluer la valeur d'une maison en fonctions des 4 critères connus
    Opérateur ternaire "?:" en C#
    Substitution de la méthode ToString
    Différences entre tableaux et listes en C#

--- Terrain/Terrain/Maison.cs
+++ Terrain/Terrain/Maison.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Terrain
 {
@@ -9,6 +10,8 @@ namespace Terrain
         public int nbPieces;
         public bool jardin;
 
+        // constructeur Maison (de la class Maison)
+        // initialisation
         public Maison(string Adresse, float Superficie, int NbPiece, bool Jardin)
         {
             adresse = Adresse;
@@ -16,5 +19,48 @@ namespace Terrain
             nbPieces = NbPiece;
             jardin = Jardin;
         }
+
+        // substitution par la méthode ToString : chaque classe hérite implicitement de la classe Object
+        // ainsi ToString retourne une représentation sous forme de chaîne de caractère de cet objet
+        // ex : toutes les variables de type int ont une méthode ToString
+
+        // méthode = fonction mais fonction =/= méthode
+        // méthode car c'est dans une classe
+        public override string ToString()
+        {
+            // ça va retourner le namespace + le nom de la class
+            //return base.ToString();
+
+            string ToString = base.ToString();
+            string toString = String.Format("Adresse = {0}\n", this.adresse);
+            toString += String.Format("Superficie = {0}m²\n", this.superficie);
+            toString += String.Format("Nombre de pièces = {0}\n", this.nbPieces);
+            toString += String.Format("Présence d'un jardin = {0}\n", this.jardin ? "Oui" : "Non");
+            toString += String.Format("> VALEUR = {0}$", this.EvaluationValeur());
+
+            return toString;
+        }
+
+        // méthode = fonction mais fonction =/= méthode
+        // méthode car c'est dans une classe
+        public float EvaluationValeur()
+        {
+            int facteur = 3000;
+
+            // this = permet de lever des ambiguïtés de nommage entre attributs et paramètres
+            // utilisé lorsqu'on ne nomme pas différent
+
+            if (this.jardin) { facteur += 500; }
+            if (this.nbPieces > 3) { facteur += 200; }
+
+            // Regex.IsMatch : permet de renvoyer une occurence qui est délimité par les \b dans la string qui suit
+            // Si Regex.IsMatch = true par exemple ici pour Paris ça va renvoyer le facteur 7000
+
+            if (Regex.IsMatch(this.adresse, @"\bParis\b")) { facteur += 7000; }
+            else if (Regex.IsMatch(this.adresse, @"\bLyon\b")) { facteur += 2000; }
+
+            return this.superficie * facteur;
+        }
+
     }
 }
--- Terrain/Terrain/Program.cs
+++ Terrain/Terrain/Program.cs
@@ -10,14 +10,18 @@ namespace Terrain
             Maison uneAutreMaison = new Maison("4 place Saint Louis, 22100 Dinan", 86.5f, 5, true);
             Maison uneDerniereMaison = new Maison("26 Boulevard Claude Lorrin, 40100 Dax", 25.2f, 2, false);
 
+            // tableau (array) du constructeur
+            // le constructeur créer l'objet
+            // une classe peut pas être appelé, lorsqu'un constructeur n'est pas créer
+            // il y en a un par défaut, en créer un supprime celui par défaut
+
             Maison[] CatalogueMaisons = new Maison[] { uneMaison, uneAutreMaison, uneDerniereMaison };
+
+            // boucle qui permet de passer tour par tour dans chaque élément du tableau ici
 
             foreach (Maison M in CatalogueMaisons)
             {
-                Console.WriteLine("Adresse = {0}", M.adresse );
-                Console.WriteLine("Superficie = {0}", M.superficie);
-                Console.WriteLine("Nombre de pièces = {0}", M.nbPieces);
-                Console.WriteLine("Présence d'un jardin = {0}", M.jardin ? "Oui" : "Non");
+                Console.WriteLine(M);
                 Console.Write("\n");
             }
         }

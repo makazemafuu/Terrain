17e96085afe7fcf47597c90c8a823104a2df42d7
Diane <102394629+makazemafuu@users.noreply.github.com>
2022-10-18 13:51:11 +0200

    Class Maison

---
+++ Terrain/Terrain/Maison.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Terrain
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Maison uneMaison = new Maison();
+
+            Console.WriteLine("Adresse = {0}", uneMaison.adresse);
+            Console.WriteLine("Superficie = {0}", uneMaison.superficie);
+            Console.WriteLine("Nombre de pièces = {0}", uneMaison.nbPieces);
+            Console.WriteLine("Présence d'un jardin = {0}", uneMaison.jardin);
+        }
+    }
+}

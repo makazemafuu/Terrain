d7d9b37b70ff9717940bc1fc3ddb3ba89b6a9d1f
Diane <102394629+makazemafuu@users.noreply.github.com>
2022-10-19 09:28:17 +0200

    Class Bien

---
+++ Terrain/Terrain/Bien.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Terrain
+{
+    public class Bien
+    {
+        public string adresse;
+        public float superficie;
+
+        public Bien(string Adresse, float Superficie)
+        {
+            adresse = Adresse;
+            superficie = Superficie;
+        }
+
+        public override string ToString()
+        {
+            string ToString = base.ToString();
+            string toString = String.Format("Adresse = {0}\n", this.adresse);
+            toString += String.Format("Superficie = {0}m²\n", this.superficie);
+
+            return toString;
+        }
+
+        public float EvaluationValeur()
+        {
+            int facteur = 3000;
+            return this.superficie * facteur;
+        }
+    }
+}
